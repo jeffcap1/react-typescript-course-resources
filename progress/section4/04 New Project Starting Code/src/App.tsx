6fdf380e971f4a0de3a786f2453de3625b919f2b
jeffcap1 <jeffcap1@gmail.com>
2024-07-02 00:17:13 -0400

    Expose API with useImperativeHanlde hook

--- progress/section4/04 New Project Starting Code/src/App.tsx
+++ progress/section4/04 New Project Starting Code/src/App.tsx
@@ -1,16 +1,20 @@
 import Input from "./components/Input.tsx";
-import Form from "./components/Form.tsx";
+import Form, { type FormHandle } from "./components/Form.tsx";
 import Button from "./components/Button.tsx";
+import { useRef } from "react";
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: string; }; // convert type to another type; use when we knows better than ts
+    const extractedData = data as { name: string; age: string }; // convert type to another type; use when we knows better than ts
     console.log(extractedData);
+    customForm.current?.clear();
   }
 
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <Button type="submit">Save</Button>
