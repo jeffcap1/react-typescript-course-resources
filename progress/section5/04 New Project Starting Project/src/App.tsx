514f157809bb94a794b27ae6c7496a3ac6bdb72b
jeffcap1 <jeffcap1@gmail.com>
2024-07-08 23:56:14 -0400

    Handling errors and loading state

--- progress/section5/04 New Project Starting Project/src/App.tsx
+++ progress/section5/04 New Project Starting Project/src/App.tsx
@@ -2,6 +2,7 @@ import { type ReactNode, useEffect, useState } from "react";
 import { get } from "./utils/http.ts";
 import BlogPosts, { BlogPost } from "./components/BlogPosts.tsx";
 import fetchingImg from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage.tsx";
 
 type RawDataBlogPost = {
   id: number;
@@ -12,22 +13,34 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        "https://jsonplaceholder.typicode.com/posts",
-      )) as RawDataBlogPost[];
+      setIsFetching(true);
 
-      const blogPosts: BlogPost[] = data.map((rawPost) => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body,
-        };
-      });
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts",
+        )) as RawDataBlogPost[];
 
-      setFetchedPosts(blogPosts);
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -35,8 +48,16 @@ function App() {
 
   let content: ReactNode;
 
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching Posts...</p>;
   }
 
   return (
